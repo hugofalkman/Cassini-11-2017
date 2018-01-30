cd302af23eeac64caebfce23ba9326704813a725
hugofalkman <hugo@falkman.net>
2018-01-30 21:38:23 +0100

    Lecture 10 multithreading complete

---
+++ Cassini-11-2017/CassiniViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CassiniViewController.swift
+//  Cassini-11-2017
+//
+//  Created by H Hugo Falkman on 2018-01-30.
+//  Copyright © 2018 H Hugo Falkman. All rights reserved.
+//
+
+import UIKit
+
+class CassiniViewController: UIViewController {
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            if let url = DemoURLs.NASA[identifier] {
+                if let imageVC = segue.destination.contents as? ImageViewController {
+                    imageVC.imageURL = url
+                    imageVC.title = (sender as? UIButton)?.currentTitle
+                }
+            }
+        }
+    }
+}
+
+extension UIViewController {
+    var contents: UIViewController {
+        if let navcon = self as? UINavigationController {
+            return navcon.visibleViewController ?? self
+        } else {
+            return self
+        }
+    }
+}
